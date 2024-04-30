e60ad7017f5b82c524da714785797615986ed16e
Maja Krajniewska <85988757+Majczikita@users.noreply.github.com>
2024-04-30 12:52:41 +0200

    basics

---
+++ src/MainMenuWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenuWindow extends JFrame {
+    MainMenuWindow(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+
+        getContentPane().setBackground(Color.black);
+        this.setVisible(true);
+    }
+
+    public void launchMainMenu(){
+
+    }
+}
