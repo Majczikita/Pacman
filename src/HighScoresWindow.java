bfa99f6fb7a270b733c2716b55bc8c54d1008b00
Maja Krajniewska <85988757+Majczikita@users.noreply.github.com>
2024-05-06 16:13:12 +0200

    appearence

--- src/HighScoresWindow.java
+++ src/HighScoresWindow.java
@@ -2,10 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Vector;
 
 public class HighScoresWindow extends JFrame{
