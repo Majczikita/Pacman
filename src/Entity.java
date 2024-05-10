300b8a2ec6e3ad1c1542e597c75884ffea4302d4
Maja Krajniewska <85988757+Majczikita@users.noreply.github.com>
2024-05-10 14:17:02 +0200

    directions

--- src/Entity.java
+++ src/Entity.java
@@ -3,18 +3,31 @@ import java.awt.*;
 
 public class Entity extends JLabel {
     protected int size;
+    protected ImageIcon icon;
+    protected int direction;
+
+    protected static final int UP = 180;
+    protected static final int RIGHT = 90;
+    protected static final int DOWN = 0;
+    protected static final int LEFT = 270;
 
     public Entity(String path, int x, int y){
-        size = Block.BLOCK_LENGTH-5;
+        size = Block.BLOCK_LENGTH;
         setBounds(x, y, size, size);
-        setIcon(loadIcon(path));
+        icon = loadIcon(path);
+        setIcon(icon);
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setVerticalAlignment(SwingConstants.CENTER);
     }
 
     public ImageIcon loadIcon(String path){
         ImageIcon icon = new ImageIcon(path);
         Image img = icon.getImage();
-        img = img.getScaledInstance(size, size, Image.SCALE_SMOOTH);
+        img = img.getScaledInstance(size-5, size-5, Image.SCALE_SMOOTH);
         icon = new ImageIcon(img);
         return icon;
+    }
+    public void printDirection(){
+        System.out.println(direction);
     }
 }
--- src/Pacman.java
+++ src/Pacman.java
@@ -1,10 +1,14 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
 
 public class Pacman extends Entity implements KeyListener {
     public Pacman(String path, int x, int y){
         super(path, x, y);
+        direction = RIGHT;
     }
 
     public int getEntitySize() {
@@ -13,35 +17,96 @@ public class Pacman extends Entity implements KeyListener {
 
     public void keyTyped(KeyEvent e) {
         switch(e.getKeyChar()){
-            case 'a': setLocation(getX()-10, getY());
+            case 'a':
+                setLocation(getX()-10, getY());
+                if(direction != LEFT){
+                    changeIconDirection(direction, LEFT);
+                    direction = LEFT;
+                }
                 break;
-            case 'd': setLocation(getX()+10, getY());
+            case 'd':
+                setLocation(getX()+10, getY());
+                if(direction != RIGHT){
+                    changeIconDirection(direction, RIGHT);
+                    direction = RIGHT;
+                }
                 break;
-            case 'w': setLocation(getX(), getY()-10);
+            case 'w':
+                setLocation(getX(), getY()-10);
+                if(direction != UP){
+                    changeIconDirection(direction, UP);
+                    direction = UP;
+                }
                 break;
-            case 's': setLocation(getX(), getY()+10);
+            case 's':
+                setLocation(getX(), getY()+10);
+                if(direction != DOWN){
+                    changeIconDirection(direction, DOWN);
+                    direction = DOWN;
+                }
                 break;
         }
-        System.out.print("X: "+getX()+" Y: "+getY());
+        printDirection();
+        System.out.println("X: "+getX()+" Y: "+getY());
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         switch(e.getKeyCode()){
-            case 37: setLocation(getX()-10, getY());
+            case 37:
+                setLocation(getX()-10, getY());
+                if(direction != LEFT) {
+                    changeIconDirection(direction, LEFT);
+                    direction = LEFT;
+                }
                 break;
-            case 39: setLocation(getX()+10, getY());
+            case 39:
+                setLocation(getX()+10, getY());
+                if(direction != RIGHT){
+                    changeIconDirection(direction, RIGHT);
+                    direction = RIGHT;
+                }
                 break;
-            case 38: setLocation(getX(), getY()-10);
+            case 38:
+                setLocation(getX(), getY()-10);
+                if(direction != UP){
+                    changeIconDirection(direction, UP);
+                    direction = UP;
+                }
                 break;
-            case 40: setLocation(getX(), getY()+10);
+            case 40:
+                setLocation(getX(), getY()+10);
+                if(direction != DOWN){
+                    changeIconDirection(direction, DOWN);
+                    direction = DOWN;
+                }
                 break;
         }
-        System.out.print("X: "+getX()+" Y: "+getY());
+        printDirection();
+        System.out.println("X: "+getX()+" Y: "+getY());
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    public void changeIconDirection(int prev, int current){
+        icon = rotateIcon(prev - current);
+        setIcon(icon);
+        System.out.println("rotate: " + (prev-current));
+    }
+    public ImageIcon rotateIcon(double degrees) {
+        BufferedImage image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = image.createGraphics();
+
+        // Rotate the image
+        AffineTransform transform = new AffineTransform();
+        transform.rotate(Math.toRadians(degrees), icon.getIconWidth() / 2, icon.getIconHeight() / 2);
+        g.drawImage(icon.getImage(), transform, null);
+        g.dispose();
+
+        // Create a new ImageIcon from the rotated image
+        return new ImageIcon(image);
     }
 }
